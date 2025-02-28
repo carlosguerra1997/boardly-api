d652734289652f50b6485c32fdbac8b71ae79c8c
Carlos Guerra Garcia <guerragcarlos97@gmail.com>
2025-02-28 08:51:25 +0100

    Added controllers, providers and imports to auth module

--- src/modules/auth/infrastructure/auth.module.ts
+++ src/modules/auth/infrastructure/auth.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
 
 import { LoginController } from '@/modules/auth/infrastructure/controllers/login.controller'
 
+import { LoginUseCase } from '@/modules/auth/application/login/login-use-case'
+
+import { JwtService } from '@/modules/auth/infrastructure/services/jwt/jwt.service'
+
 @Module({
-  controllers:  [LoginController]
+  controllers:  [LoginController],
+  providers: [
+    LoginUseCase,
+    JwtService
+  ],
+  imports: [ JwtModule ]
 })
 export class AuthModule {}
