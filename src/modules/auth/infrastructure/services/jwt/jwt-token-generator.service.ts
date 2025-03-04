e451e136aa135dd8d65b3d947336619e6ef577ec
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-04 20:49:26 +0100

    Implemented JWT token generator service

---
+++ src/modules/auth/infrastructure/services/jwt/jwt-token-generator.service.ts
@@ -0,0 +1,53 @@
+import { ConfigService } from '@nestjs/config'
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
+import { JwtService as JwtLibService } from '@nestjs/jwt'
+
+import { Token } from '@/config/Configuration'
+
+import { TokenGenerator } from '@/modules/auth/ports/token-generator.interface'
+
+import { UnauthorizedException } from '@/modules/auth/application/exceptions/unauthorized-exception'
+
+@Injectable()
+export class JwtService implements TokenGenerator {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtLibService
+  ) {}
+
+  async generate(userId: string): Promise<{ accessToken: string, refreshToken: string }> {
+    const accessToken = await this.generateAccessToken(userId)
+    const refreshToken = await this.generateRefreshToken(userId)
+
+    return { accessToken, refreshToken }
+  }
+
+  private async generateAccessToken(userId: string): Promise<string> {
+    const accessTokenConfig = this.configService.get<Token>('auth.accessToken')
+
+    if (!accessTokenConfig || !accessTokenConfig.secret) {
+      throw new InternalServerErrorException('Access token could not be created')
+    }
+
+    return this.jwtService.sign({ userId }, accessTokenConfig)
+  }
+
+  private async generateRefreshToken(userId: string): Promise<string> {
+    const refreshTokenConfig = this.configService.get<Token>('auth.refreshToken')
+
+    if (!refreshTokenConfig || !refreshTokenConfig.secret) {
+      throw new InternalServerErrorException('Refresh token could not be created')
+    }
+
+    return this.jwtService.sign({ userId }, refreshTokenConfig)
+  }
+
+  async verifyToken(token: string, type: 'access' | 'refresh'): Promise<any> {
+    try {
+      const tokenSecret = this.configService.get<string>(`auth.${type}Token.secret`)
+      return this.jwtService.verify(token, { secret: tokenSecret})
+    } catch (error) {
+      throw new UnauthorizedException()
+    }
+  }
+}
