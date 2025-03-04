a53cc8447ab3e86b1d4a16991ce0937c685ad4ec
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-04 20:49:13 +0100

    Small fixes

--- src/modules/auth/infrastructure/services/hasher/argon-hasher.service.ts
+++ src/modules/auth/infrastructure/services/hasher/argon-hasher.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common'
 
 import { hash, verify } from 'argon2'
 
+import { Hasher } from '@/common/domain/identity/hasher.interface'
+
 import { PasswordHashingException } from '@/modules/auth/application/exceptions/password-hashing-exception'
 import { PasswordVerifyingException } from '@/modules/auth/application/exceptions/password-verifying-exception'
 
 @Injectable()
-export class ArgonHasherService {
+export class ArgonHasherService implements Hasher {
   async hash(item: string): Promise<string> {
     try {
       const hashedItem = await hash(item)
@@ -16,7 +18,7 @@ export class ArgonHasherService {
     }
   }
 
-  async compare(item: string, hashedItem: string): Promise<boolean> {
+  async compare(hashedItem: string, item: string): Promise<boolean> {
     try {
       const matched = await verify(hashedItem, item)
       return matched
