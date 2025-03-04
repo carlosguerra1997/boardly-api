272827f0d8c6f082bd929524898fe41ac1f04abb
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-04 20:48:49 +0100

    Deleted uneccessary file

--- src/common/infrastructure/services/id-generator/argon-id-generator.service.ts
+++
@@ -1,9 +0,0 @@
-import { randomUUID } from 'crypto'
-
-import { IdGenerator } from '@/common/domain/identity/id-generator.interface'
-
-export class ArgonIdGenerator implements IdGenerator {
-  generate(): string {
-    return randomUUID()
-  }
-}
