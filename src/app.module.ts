d4d544cb7fa527aa347d85ac8ce86739a2293804
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-04 20:40:56 +0100

    App & Common Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from '@/modules/auth/infrastructure/auth.module'
 import { CommonModule } from '@/common/common.module'
+import { UserModule } from '@/modules/user/infrastructure/user.module'
 
 @Module({
   imports: [
     AuthModule,
-    CommonModule
+    CommonModule,
+    UserModule
   ]
 })
 export class AppModule {}
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,19 +1,39 @@
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 import config from '@/config/index'
+import { Configuration } from '@/config/Configuration'
 
-import { Hasher } from '@/common/domain/identity/hasher'
+import { Hasher } from '@/common/domain/identity/hasher.interface'
+import { IdGenerator } from '@/common/domain/identity/id-generator.interface'
+
+import { ArgonHasherService } from '@/modules/auth/infrastructure/services/hasher/argon-hasher.service'
+import { CryptoIdGenerator } from '@/common/infrastructure/services/id-generator/crypto-id-generator.service'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [config],
       isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService<Configuration>) => {
+        const databaseConfig = config.get('database')
+        return databaseConfig
+      },
+      inject: [ConfigService],
     })
   ],
+  providers: [
+    { provide: Hasher, useClass: ArgonHasherService },
+    { provide: IdGenerator, useClass: CryptoIdGenerator }
+  ],
   exports: [
-    Hasher
+    ConfigModule,
+    Hasher,
+    IdGenerator,
   ]
 })
 export class CommonModule {}
