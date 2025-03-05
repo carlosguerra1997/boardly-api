1aae130fa950e4abeaeade61aa9a75fd8df7846e
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-05 18:05:37 +0100

    Getting env variables from process.env out of function

--- config/environments/base.ts
+++ config/environments/base.ts
@@ -1,26 +1,39 @@
 import { Configuration } from '@/config/Configuration'
 
-const databasePort: string = process.env.DATABASE_PORT as string
+
+const {
+  ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRATION,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRATION,
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_NAME,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD
+} = process.env
+
+const databasePort: string = DATABASE_PORT as string
 
 export const getBaseConfig = (): Configuration => ({
   env: 'dev',
   auth: {
     accessToken: {
-      secret: process.env.ACCESS_TOKEN_SECRET || 'access_secret_dev',
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRATION ||'15m',
+      secret: ACCESS_TOKEN_SECRET || 'access_secret_dev',
+      expiresIn: ACCESS_TOKEN_EXPIRATION ||'15m',
     },
     refreshToken: {
-      secret: process.env.REFRESH_TOKEN_SECRET || 'refresh_secret_dev',
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRATION || '7d'
+      secret: REFRESH_TOKEN_SECRET || 'refresh_secret_dev',
+      expiresIn: REFRESH_TOKEN_EXPIRATION || '7d'
     }
   },
   database: {
     type: 'postgres',
-    host: process.env.DATABASE_HOST || 'database',
+    host: DATABASE_HOST || 'database',
     port: parseInt(databasePort) || 5432,
-    database: process.env.DATABASE_NAME || 'boardly',
-    username: process.env.DATABASE_USERNAME || 'boardly-user',
-    password: process.env.DATABASE_PASSWORD || 'boardly-root',
+    database: DATABASE_NAME || 'boardly',
+    username: DATABASE_USERNAME || 'boardly-user',
+    password: DATABASE_PASSWORD || 'boardly-root',
     autoLoadEntities: true,
     keepConnectionAlive: true,
     logging: false
