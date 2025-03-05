27fe0cb8a84fd12c7ddc3bd29ea6cdf0b0eb180e
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-05 18:06:16 +0100

    Add http exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
-import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core'
+import { Module } from '@nestjs/common'
+
+import { HttpExceptionFilter } from '@/common/infrastructure/interceptors/http-exception-filter'
 
 import { AuthModule } from '@/modules/auth/infrastructure/auth.module'
 import { CommonModule } from '@/common/common.module'
@@ -9,6 +12,9 @@ import { UserModule } from '@/modules/user/infrastructure/user.module'
     AuthModule,
     CommonModule,
     UserModule
+  ],
+  providers: [
+    { provide: APP_FILTER, useClass: HttpExceptionFilter }
   ]
 })
 export class AppModule {}
