51c9033e5138153a721e41f389b7e61eef50a9ec
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-06 20:09:15 +0100

    Implemented JwtStrategy for passport

--- src/modules/auth/infrastructure/strategies/jwt.strategy.ts
+++ src/modules/auth/infrastructure/strategies/jwt.strategy.ts
@@ -1,6 +1,39 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+
+import { User } from '@/modules/user/domain/user'
+import { UserRepository } from '@/modules/user/domain/user-repository'
+
+interface PassportValidatePayload {
+  userId: string
+  iat: number
+  exp: number
+}
 
 @Injectable()
-export class JwtStrategy {
-  constructor() {}
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    @Inject(UserRepository) private userRepo: UserRepository
+  ) {
+    const accessTokenSecret = configService.get('auth.accessToken.secret') as string
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: accessTokenSecret
+    })
+  }
+
+  async validate(payload: PassportValidatePayload): Promise<User> {
+    const user = await this.userRepo.obtainById(payload.userId)
+
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+
+    return user
+  }
 }
