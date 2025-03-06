257e788b2406ed4603d39e7f1f1e01df1f59f1e2
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-06 20:04:39 +0100

    Moved payloads to use cases layer

---
+++ src/modules/auth/application/login/login-payload.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod'
+
+export const loginPayloadSchema = z.object({
+  email: z
+    .string({ message: 'Email not valid' })
+    .email({ message: 'Invalid email' }),
+  password: z
+    .string({ message: 'Password not valid' })
+})
+
+export type LoginPayload = z.infer<typeof loginPayloadSchema>
