57efa0c52bf57d68576148e731fc3d1e926e5dce
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-09 20:07:38 +0100

    Using cookie parser middleware to get cookies in request

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
+import cookieParser from 'cookie-parser'
 
 import { AppModule } from 'src/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  app.use(cookieParser())
 
+  await app.listen(3000)
   console.log(`🚀 API running on http://localhost:3000`);
 }
 
