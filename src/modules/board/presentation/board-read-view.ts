9442474b270a39784bbb585e8497753fb8354e17
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-10 20:49:18 +0100

    Using board response as type

--- src/modules/board/presentation/board-read-view.ts
+++ src/modules/board/presentation/board-read-view.ts
@@ -1,10 +1,10 @@
 import { Result } from '@/common/domain/identity/result'
 
-import { Board } from '@/modules/board/domain/board'
+import { BoardResponse } from '@/modules/board/application/response/board-response'
 import { boardSerialize } from '@/modules/board/presentation/board-serialize'
 
 export class BoardReadView extends Result {
-  constructor(board: Board) {
+  constructor(board: BoardResponse) {
     super(Result.successMessage)
     this.data = boardSerialize(board)
   }
