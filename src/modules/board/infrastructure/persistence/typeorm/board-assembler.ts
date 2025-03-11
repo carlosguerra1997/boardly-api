021a379f94c401c3d268e645984fa31d52e7239d
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-11 20:09:15 +0100

    Implement board assembler

---
+++ src/modules/board/infrastructure/persistence/typeorm/board-assembler.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common'
+
+import { Board } from '@/modules/board/domain/board'
+import { BoardAssembler as IBoardAssembler } from '@/modules/board/domain/board-assembler'
+
+import { BoardSchemaInterface } from '@/modules/board/infrastructure/persistence/typeorm/mapping/board-schema'
+
+@Injectable()
+export class BoardAssembler implements IBoardAssembler<Board, BoardSchemaInterface> {
+  toDatabase(item: Board): BoardSchemaInterface {
+    return {
+      id: item.getId(),
+      name: item.getName(),
+      description: item.getDescription(),
+      status: item.getStatus().getStatus(),
+      createdAt: item.getCreatedAt(),
+      updatedAt: item.getUpdatedAt()
+    }
+  }
+
+  toEntity(item: BoardSchemaInterface): Board {
+    return new Board(
+      item.id,
+      item.name,
+      item.description,
+      item.status
+    )
+  }
+}
