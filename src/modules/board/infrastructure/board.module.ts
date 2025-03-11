350993de2161e42922ed9b9aaec6f019b808efbd
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-11 20:07:33 +0100

    Importing necessary providers

--- src/modules/board/infrastructure/board.module.ts
+++ src/modules/board/infrastructure/board.module.ts
@@ -4,10 +4,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '@/modules/auth/infrastructure/auth.module';
 import { CommonModule } from '@/common/common.module'
 
+import { BoardAssembler as IBoardAssembler } from '@/modules/board/domain/board-assembler'
+import { BoardRepository } from '@/modules/board/domain/board-repository'
+
 import { BoardCreateController } from '@/modules/board/infrastructure/controllers/v1/board-create.controller'
 import { BoardCreateUseCase } from '@/modules/board/application/creator/board-create-use-case'
 
+import { BoardAssembler } from '@/modules/board/infrastructure/persistence/typeorm/board-assembler'
 import { BoardSchema } from '@/modules/board/infrastructure/persistence/typeorm/mapping/board-schema'
+import { PostgresBoardRepository } from '@/modules/board/infrastructure/persistence/typeorm/postgres-board-repository'
+
 
 @Module({
   imports: [
@@ -19,7 +25,9 @@ import { BoardSchema } from '@/modules/board/infrastructure/persistence/typeorm/
     BoardCreateController
   ],
   providers: [
-    BoardCreateUseCase
+    BoardCreateUseCase,
+    { provide: IBoardAssembler, useClass: BoardAssembler },
+    { provide: BoardRepository, useClass: PostgresBoardRepository }
   ]
 })
 export class BoardModule {}
