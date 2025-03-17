d80a23c983c18b4ec4db88c3647e87ffc1b825ef
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-17 18:17:49 +0100

    Changed name for board create payload

--- src/modules/board/application/creator/board-create-payload.ts
+++ src/modules/board/application/creator/board-create-payload.ts
@@ -1,11 +1,12 @@
 import { z } from 'zod'
 
-export const boardPayloadValidationSchema = z.object({
+export const boardCreatePayloadValidationSchema = z.object({
   name: z
     .string({ message: 'Name not valid' })
     .nonempty({ message: 'Name cannot be empty' }),
   description: z
     .string({ message: 'Description not valid' })
+    .optional()
 })
 
-export type BoardCreatePayload = z.infer<typeof boardPayloadValidationSchema>
+export type BoardCreatePayload = z.infer<typeof boardCreatePayloadValidationSchema>
