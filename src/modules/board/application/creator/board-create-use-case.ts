f2ada1335f5981a4e5484fcb5ca40192389a9a0a
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-17 18:22:55 +0100

    feat: creating board from static method

--- src/modules/board/application/creator/board-create-use-case.ts
+++ src/modules/board/application/creator/board-create-use-case.ts
@@ -1,10 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common'
 
+import { Board } from '@/modules/board/domain/board'
 import { BoardRepository } from '@/modules/board/domain/board-repository'
 import { IdGenerator } from '@/common/domain/identity/id-generator'
 
 import { type BoardCreatePayload } from '@/modules/board/application/creator/board-create-payload'
-import { BoardRequest } from '@/modules/board/application/request/board-request'
 import { BoardResponse } from '@/modules/board/application/response/board-response'
 
 @Injectable()
@@ -18,10 +18,15 @@ export class BoardCreateUseCase {
     payload: BoardCreatePayload, 
     userId: string
   ): Promise<BoardResponse> {
+    const { name, description } = payload
     const id = await this.idGenerator.generate()
 
-    const request = new BoardRequest(id, payload)
-    const board = request.make()
+    const board = Board.create(
+      id,
+      name,
+      description ?? '',
+    )
+    
     await this.boardRepo.save(board)
 
     const boardResponse = new BoardResponse(
