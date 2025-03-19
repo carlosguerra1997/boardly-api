30bcf4326822bc411c903ecb517029597f0f1b2e
Carlos Guerra <cguerra.clb@grupoprisa.com>
2025-03-19 17:55:39 +0100

    Board read controller & use case

---
+++ src/modules/board/application/reader/board-read-use-case.ts
@@ -0,0 +1,29 @@
+import { Inject, Injectable } from '@nestjs/common'
+
+import { BoardRepository } from '@/modules/board/domain/board-repository'
+
+import { BoardNotFoundException } from '@/modules/board/application/exceptions/BoardNotFoundException'
+import { BoardResponse } from '@/modules/board/application/response/board-response'
+
+@Injectable()
+export class BoardReadUseCase {
+  constructor(
+    @Inject(BoardRepository) private boardRepo: BoardRepository
+  ) {}
+
+  async dispatch(id: string): Promise<BoardResponse> {
+    const board = await this.boardRepo.obtainById(id)
+    if (!board) {
+      throw new BoardNotFoundException()
+    }
+
+    const boardResponse = new BoardResponse(
+      board.getId(),
+      board.getName(),
+      board.getDescription(),
+      board.getStatus().getStatus()
+    )
+
+    return boardResponse
+  }
+}
---
+++ src/modules/board/infrastructure/controllers/v1/board-read.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, InternalServerErrorException, NotFoundException, Param, UseGuards } from '@nestjs/common'
+
+import { NotFoundError } from '@/common/domain/identity/exception/not-found-error'
+import { Result } from '@/common/domain/identity/result'
+
+import { BoardReadUseCase } from '@/modules/board/application/reader/board-read-use-case'
+
+import { JwtAccessTokenGuard } from '@/modules/auth/infrastructure/guard/jwt-access-token.guard'
+
+import { BoardReadView } from '@/modules/board/presentation/board-read-view'
+
+@Controller('v1')
+export class BoardReadController {
+  constructor(
+    private reader: BoardReadUseCase
+  ) {}
+
+  @Get('board/:id')
+  @UseGuards(JwtAccessTokenGuard)
+  async invoke(@Param('id') id: string): Promise<Result> {
+    try {
+      const response = await this.reader.dispatch(id)
+      const result = new BoardReadView(response)
+      return result
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw new NotFoundException(error.message)
+      }
+
+      throw new InternalServerErrorException
+    }
+  }
+}
