5d22b0b16b3ee376436a360c40c9c7e60d677e64
Carlos Guerra <cguerra.clb@grupoprisa.com>
2025-03-21 08:25:34 +0100

    feat: add BoardQueryPayload interface for querying boards with status and visibility

---
+++ src/modules/board/application/lister/board-query-payload.ts
@@ -0,0 +1,6 @@
+import { ListQueryPayload } from '@/common/domain/identity/list/list-query-payload'
+
+export interface BoardQueryPayload extends ListQueryPayload {
+  status: string
+  visibility: string
+}
