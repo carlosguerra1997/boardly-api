573208942ba83fa14debce98d7d089ff2c6aafc0
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-24 19:00:47 +0100

    feat: update ListRepository to use ListQuery for obtaining items by query

--- src/common/domain/identity/list-repository.ts
+++ src/common/domain/identity/list-repository.ts
@@ -1,6 +1,7 @@
+import { ListQuery } from '@/common/domain/identity/list/list-query'
 import { StoredRepository } from '@/common/domain/identity/stored-repository'
 
 export interface ListRepository<T> extends StoredRepository<T> {
   obtainById(id: string): Promise<T | null>
-  obtainByQuery(query?: any): Promise<T[]>
+  obtainByQuery(query: ListQuery): Promise<T[]>
 }
