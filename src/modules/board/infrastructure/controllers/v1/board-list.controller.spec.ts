bfcb1dc9c0c031d62e8b70ee87d32c1552ac309e
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-26 19:27:03 +0100

    test: refactor board list controller tests to use mocks and improve assertions

--- src/modules/board/infrastructure/controllers/v1/board-list.controller.spec.ts
+++ src/modules/board/infrastructure/controllers/v1/board-list.controller.spec.ts
@@ -1,98 +1,60 @@
-import { InternalServerErrorException } from '@nestjs/common'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 
 import { BoardListUseCase } from '@/modules/board/application/lister/board-list-use-case'
 
-import { BoardRepositoryStub } from '@/tests/doubles/persistence/board-repository-stub'
 import { BoardListController } from '@/modules/board/infrastructure/controllers/v1/board-list.controller'
 
+import { BoardListView } from '@/modules/board/presentation/board-list-view'
+
+import { getBoardFixtures } from '@/tests/fixtures/board-fixture'
+import { BoardQueryPayload } from '@/modules/board/application/lister/board-query-payload'
+import { InternalServerErrorException } from '@nestjs/common'
+
 describe('Board list controller test suite', () => {
-  let repo: BoardRepositoryStub
-  let lister: BoardListUseCase
   let controller: BoardListController
+  let listerMock: BoardListUseCase
 
   beforeEach(() => {
-    repo = new BoardRepositoryStub()
-    lister = new BoardListUseCase(repo)
-    controller = new BoardListController(lister)
+    listerMock = {
+      dispatch: vi.fn()
+    } as unknown as BoardListUseCase
+
+    controller = new BoardListController(listerMock)
   })
 
-  it ('Should list all boards', async () => {
-    repo.all()
-
-    const result = await controller.invoke({})
-    
-    expect(result.message).toBe('success')
-    expect(result.data).toHaveLength(2)
-  })
-
-  it('Should return boards sorted by default sort field', async () => {
-    repo.all()
+  it('Should return a list of boards', async () => {
+    const boardsMock = getBoardFixtures()
+    vi.spyOn(listerMock, 'dispatch')
+      .mockResolvedValue(boardsMock)
 
     const result = await controller.invoke({})
 
+    expect(listerMock.dispatch).toHaveBeenCalled()
+    expect(result).toBeInstanceOf(BoardListView)
     expect(result.message).toBe('success')
     expect(result.data).toHaveLength(2)
-    expect(result.data[0].name).toBe('Testing board 1')
-    expect(repo.query?.sort.size).toBe(1)
-    expect(repo.query?.sort.get('createdAt')).toBeDefined()
+    expect(result.data[1].name).toBe('Testing board 2')
   })
 
-  it('Should return boards sorted by name asc', async () => {
-    repo.all()
+  it('Should call lister dispatch function with correct query parameters', async () => {
+    const boardsMock = getBoardFixtures()
+    vi.spyOn(listerMock, 'dispatch')
+      .mockResolvedValue(boardsMock)
 
-    const result = await controller.invoke({ sort: 'name:asc' })
+    const query: BoardQueryPayload = { status: 'active', visibility: 'private' }
+    await controller.invoke(query)
 
-    expect(result.message).toBe('success')
-    expect(result.data).toHaveLength(2)
-    expect(result.data[0].name).toBe('Testing board 1')
-    expect(repo.query?.sort.size).toBe(1)
-    expect(repo.query?.sort.get('name')).toBeDefined()
+    expect(listerMock.dispatch).toHaveBeenCalledWith(query)
   })
 
-  it('Should return boards filtered by status', async () => {
-    repo.all()
+  it('Should throw InternalServerException if use case throws unexpected error', async () => {  
+    vi.spyOn(listerMock, 'dispatch')
+      .mockRejectedValue(new Error('Unexpected error'))
 
-    const result = await controller.invoke({ status: 'active' })
-
-    expect(result.message).toBe('success')
-    expect(result.data).toHaveLength(1)
-    expect(result.data[0].name).toBe('Testing board 1')
-    expect(repo.query?.filters.size).toBe(1)
-    expect(repo.query?.filters.get('status')).toBeDefined()
-  })
-
-  it('Should return boards filtered by visibility', async () => {
-    repo.all()
-
-    const result = await controller.invoke({ visibility: 'private' })
-
-    expect(result.message).toBe('success')
-    expect(result.data).toHaveLength(2)
-    expect(result.data[0].name).toBe('Testing board 1')
-    expect(repo.query?.filters.size).toBe(1)
-    expect(repo.query?.filters.get('visibility')).toBeDefined()
-  })
-
-  it('Should return boards filtered by status and visibility', async () => {
-    repo.all()
-
-    const result = await controller.invoke({ status: 'active', visibility: 'private' })
-
-    expect(result.message).toBe('success')
-    expect(result.data).toHaveLength(1)
-    expect(result.data[0].name).toBe('Testing board 1')
-    expect(repo.query?.filters.size).toBe(2)
-  })
-
-  it('Should throw InternalServerException when unexpected error', async () => {
-    repo.error = 'Unexpected error'
-
-    await controller
-      .invoke({})
+    await controller.invoke({})
       .catch(error => {
         expect(error).toBeInstanceOf(InternalServerErrorException)
-        expect(error.message).toBe('Internal Server Error')
+        expect(error.message).toBe('Internal Server Error') 
       })
   })
 })
