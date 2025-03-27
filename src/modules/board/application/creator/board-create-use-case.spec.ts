7c68efc14f1a10bafda428eb74fff3267d258f50
carlosguerra1997 <guerragcarlos97@gmail.com>
2025-03-27 21:50:32 +0100

    test: enhance board-create-use-case tests with additional assertions and clarity

--- src/modules/board/application/creator/board-create-use-case.spec.ts
+++ src/modules/board/application/creator/board-create-use-case.spec.ts
@@ -9,7 +9,7 @@ import { IdGeneratorStub } from '@/tests/doubles/common/id-generator-stub'
 import { BoardRepositoryStub } from '@/tests/doubles/persistence/board-repository-stub'
 import { BoardPayloadMother } from '@/tests/mothers/board-payload-mother'
 
-describe('Board create use case test suite', () => {
+describe('board-create-use-case test suite', () => {
   let idGenerator: IdGeneratorStub
 
   let creator: BoardCreateUseCase
@@ -22,7 +22,7 @@ describe('Board create use case test suite', () => {
     creator = new BoardCreateUseCase(idGenerator, repo)
   })
 
-  it('Should call ID generator service correctly', async () => {
+  it('Should call ID generator service only once', async () => {
     const payload = BoardPayloadMother.create()
     vi.spyOn(idGenerator, 'generate')
 
@@ -32,11 +32,22 @@ describe('Board create use case test suite', () => {
     expect(idGenerator.generate).toHaveBeenCalledOnce()
   })
 
+  it('Should call repository save method only once', async () => {
+    const payload = BoardPayloadMother.create()
+    vi.spyOn(repo, 'save')
+
+    const userId = 'test-id'
+    await creator.dispatch(payload, userId)
+
+    expect(repo.save).toHaveBeenCalledTimes(1)
+  })
+
   it('Should create a board successfully', async () => {
     const payload = BoardPayloadMother.create()
     vi.spyOn(repo, 'save')
 
-    const board = await creator.dispatch(payload, 'test-id')
+    const userId = 'test-id'
+    const board = await creator.dispatch(payload, userId)
 
     expect(repo.save).toHaveBeenCalledWith(board)
     expect(board.getId()).toBe('mocked-id')
@@ -47,11 +58,22 @@ describe('Board create use case test suite', () => {
   it('Should create a board with correct visibility and status', async () => {
     const payload = BoardPayloadMother.create()
 
-    const board = await creator.dispatch(payload, 'test-id')
+    const userId = 'test-id'
+    const board = await creator.dispatch(payload, userId)
 
     expect(board.getStatus()).toBeInstanceOf(BoardStatus)
     expect(board.getVisibility()).toBeInstanceOf(BoardVisibility)
     expect(board.getStatus().getValue()).toBe('active')
     expect(board.getVisibility().getValue()).toBe('private')
+  })
+
+  it('Should create a board with an empty description when not provided', async () => {
+    const payload = BoardPayloadMother.withoutDescription()
+    
+    const usedId = 'test-id'
+    const board = await creator.dispatch(payload, usedId)
+
+    expect(board.getDescription()).toBeDefined()
+    expect(board.getDescription()).toBe('')
   })
 })
